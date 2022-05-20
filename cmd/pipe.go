e0afb7c6d353f64a8e97d3c2025708968afb04ef
lingyin <dmzlingyin@163.com>
2022-05-20 18:33:39 +0800

    fix syntax error

--- cmd/pipe.go
+++ cmd/pipe.go
@@ -9,7 +9,7 @@ import (
 	"locr/server"
 )
 
-// RecoPipe 适用于一次性识别图片内容(无剪贴板), 结合管道符, 将识别结果输出的文件
+// RecoPipe 适用于一次性识别图片内容(无剪贴板), 结合管道符, 将识别结果输出到文件
 func RecoPipe(r io.Reader) {
 	b, err := io.ReadAll(r)
 	if err != nil {
