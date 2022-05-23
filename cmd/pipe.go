eb70276ccee598a6ea12e2e6a4dc3e73428026bd
dmzlingyin <dmzlingyin@163.com>
2022-05-23 20:58:00 +0800

    fix pipe.go bug

--- cmd/pipe.go
+++ cmd/pipe.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"fmt"
 	"io"
+	"os"
 
 	"locr/pkg/log"
 	"locr/pkg/utils"
@@ -14,16 +15,25 @@ func RecoPipe(r io.Reader) {
 	b, err := io.ReadAll(r)
 	if err != nil {
 		log.ErrorLogger.Println(err)
+		exit(err)
 	}
+
 	if utils.ImageType(b) == -1 {
 		log.WarningLogger.Println("File is not an image.")
+		fmt.Fprintln(os.Stderr, "File is not an image.")
 		return
 	}
 
 	res, err := server.RecoBase64(b)
 	if err != nil {
 		log.ErrorLogger.Println(err)
+		exit(err)
 	}
 
 	fmt.Println(utils.ExtractText(res))
+}
+
+func exit(err error) {
+	fmt.Fprintln(os.Stderr, err)
+	os.Exit(1)
 }
